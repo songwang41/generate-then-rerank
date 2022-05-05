cdf849f47a48f04115c8187d3f1363d186fe7b54
Weizhou Shen (FA Talent) <v-wshen@microsoft.com>
2022-05-05 14:20:43 +0800

    5/5/2022

---
+++ ranker/BertRanker_v1/run_prepro_xsum.sh
@@ -0,0 +1,3 @@
+python preprocess.py --data_dir ../GANRanker/data/glge_new/xsum --dataset_name xsum \
+    --candidate_dir /weizhou_data/generation/bart-samsum/bart-universal-generation/results/xsum-large \
+    --num_cand 16 --save_name xsum-large
